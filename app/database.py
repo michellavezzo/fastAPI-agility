22e257c1369b7883feea81802ca2cc3c53274203
Michel Lavezzo <michellavezzo@gmail.com>
2025-06-12 12:52:18 -0300

    refactor: change DB name

--- app/database.py
+++ app/database.py
@@ -1,9 +1,7 @@
-# app/database.py
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-DATABASE_URL = "sqlite:///./users.db"
+DATABASE_URL = "sqlite:///./agility.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
